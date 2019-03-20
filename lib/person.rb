d48a6c94e09d4c25879807badf612d611c17139a
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-20 19:21:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,4 +2,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height
   attr_accessor :weight, :handed, :complexion, :t_shirt_size
   attr_accessor :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+  end
 end
